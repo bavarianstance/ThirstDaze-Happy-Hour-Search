ef741598bf2c03b08c6ae0ec8ae3708979b3d3d6
bavarianstance <bavarianstance@gmail.com>
2019-03-13 23:34:50 -0700

    attempted fix for thumbs btns

--- assets/js/app.js
+++ assets/js/app.js
@@ -159,7 +159,7 @@ $(document).ready(function () {
                 function writeBusinessData(businessId, name) {
                     database.ref().on("value", function (snapshot) {
 
-                        if (snapshot.child("businessId") === null) {
+                        if (firebase.database().ref('businesses/' + businessId) === null) {
 
                             firebase.database().ref('businesses/' + businessId).set({
                                 name: name,
